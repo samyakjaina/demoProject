9b8a787929f5b0767874a2ef943a76c66be78be0
samjain109 <samyakjain2732000@gmail.com>
2024-03-16 20:45:23 +0530

    main branch commit

--- src/main/java/com/example/demo/constant/ResponseMessage.java
+++ src/main/java/com/example/demo/constant/ResponseMessage.java
@@ -2,7 +2,7 @@ package com.example.demo.constant;
 
 public enum ResponseMessage {
 
-	Null("No Data Found updated cah");
+	Null("No Data Found updated main");
 	private String message;
 	private ResponseMessage( String message) {
 		this.message = message;
