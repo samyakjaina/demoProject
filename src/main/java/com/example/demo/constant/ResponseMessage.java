92b2c22a01715de3d5c003c96a13dd2223e10a39
samjain109 <samyakjain2732000@gmail.com>
2024-03-16 19:35:53 +0530

    updated the message

--- src/main/java/com/example/demo/constant/ResponseMessage.java
+++ src/main/java/com/example/demo/constant/ResponseMessage.java
@@ -2,7 +2,7 @@ package com.example.demo.constant;
 
 public enum ResponseMessage {
 
-	Null("No Data Found");
+	Null("No Data Found updated data ");
 	private String message;
 	private ResponseMessage( String message) {
 		this.message = message;
